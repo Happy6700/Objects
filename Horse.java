df5eb1842cb9c85dedb70ab2102f0213ca2ec60f
Happy <manncovey@gmail.com>
2024-09-27 11:25:00 -0600

    Add files via upload

---
+++ Horse.java
@@ -0,0 +1,11 @@
+/**
+ * Madison Covey
+ */
+public class Horse {
+    public static void main(String[] args) {
+        String[] Horse = {"Ginger,2008," , "Trigger,2000" , 
+        "blackBeauty,1877" , "Mr. Ed,1961" } ;
+    }
+
+    
+}
